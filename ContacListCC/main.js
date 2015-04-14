802efa5bfaa20540519174562a4274af8c99061d
asglasby <aisha.developer@gmail.com>
2015-04-14 11:47:06 -0400

    added CRUD functions

--- ContacListCC/main.js
+++ ContacListCC/main.js
@@ -15,8 +15,46 @@ contacts.push(new Contact("Spidey", "spidey@gmail.com", "803-123-4523"));
 var listContacts = function () {
     document.getElementById('displayContacts').innerHTML = " ";
     for (var i = 0; i < contacts.length; i++) {
-        document.getElementById('displayContacts').innerHTML += '<tr><td>' + contacts[i].name + '</td><td>' + contacts[i].email + '</td><td>' + contacts[i].phone + '</td></tr>';
+        document.getElementById('displayContacts').innerHTML += '<tr><td id="name' + i + '">' + contacts[i].name + '</td><td id="email' + i + '">' + contacts[i].email + '</td><td id="phone' + i + '">' + contacts[i].phone + '</td><td><button class="btn btn-warning" onclick=updateContact(' + i + ')>Update</button></div><button class="btn btn-danger" onclick=deleteContact(' + i + ')>Delete</button></td></tr>';
     }
+}
+
+var addNewContact = function () {
+    var name = document.getElementById('inputName').value;
+    var email = document.getElementById('inputEmail').value;
+    var phone = document.getElementById('inputPhone').value;
+    var contact = new Contact(name, email, phone);
+    contacts.push(contact);
+    listContacts();
+}
+
+var deleteContact = function (i) {
+    contacts.splice(i, 1);
+    listContacts();
+}
+
+var updateContact = function (i) {
+    // alert('not working yet');
+    var nameId = "name" + i;
+    var emailId = "email" + i;
+    var phoneId = "phone" + i;
+
+    document.getElementById(nameId).innerHTML = '<input value="' + contacts[i].name + '" class="form-control" id="name' + i + '" />';
+    document.getElementById(emailId).innerHTML = '<input value="' + contacts[i].email + '" class="form-control" id="email' + i + '" />';
+    document.getElementById(phoneId).innerHTML = '<input value="' + contacts[i].phone + '" class="form-control" id="phone' + i + '" /><br /><button id="updateButton" class="btn btn-warning" onclick=submitUpdatedContact(' + i + ')>Submit</button>';
+}
+
+var submitUpdatedContact = function (i) {
+    // alert("not working just yet")
+    var nameUId = "name" + i;
+    var emailUId = "email" + i;
+    var phoneUId = "phone" + i;
+
+    contacts[i].name = document.getElementById(nameUId).value;
+    contacts[i].email = document.getElementById(emailUId).value;
+    contacts[i].phone = document.getElementById(phoneUId).value;
+
+    listContacts();
 }
 
 
